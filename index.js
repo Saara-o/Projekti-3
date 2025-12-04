9475bcbcf53a7244ca37885fc7a2ee5354388092
Saara <saara.orvas@student.laurea.fi>
2025-12-04 13:46:48 +0200

    Lisätty kategoria -painikkeet  data-attribuutilla jQuerya varten. Lisätty click -tapahtuma ja uusi axios-kutsu kategorioiden mukaan. Lisätty Google fontin nuoli-ikoni ja viimeistely ulkoasu.

--- index.js
+++ index.js
@@ -1,7 +1,82 @@
 // Oletuksena haetaan reseptin nimen mukaan
 let hakutyyppi = "strMeal"
 
-// API -haku tapahtuu Enter-näppäintä painamalla, haetaan reseptin nimen mukaan
+// Reseptien hakeminen kategoriat-painikkeilla
+$(document).ready(function() {
+    $(".hakutyyppiBtn").click(function () {
+
+        // Haetaan kategoria data-attribuutista
+        let kategoria = $(this).data("kategoria");
+
+        // Jos painike on aktiivinen, poistetaan aktiivisuus
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+            // Tyhjennetään tulokset, kun painike ei ole enää aktiivinen
+            $("#tulokset").empty();
+            return;
+        
+        // Muuten vaihdetaan aktiivinen painike
+        } else {
+            $(".hakutyyppiBtn").removeClass("active");
+            $(this).addClass("active");
+        }
+        
+        // Luodaan URL kategorioihin
+        let url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=" + encodeURIComponent(kategoria);
+
+        // Axios-pyyntö
+        axios.get(url)
+            .then(function(response) {
+                let data = response.data;
+
+                if (!data.meals) {
+                    $("#tulokset").text("No recipes found. Try another category.");
+                    return;
+                }
+
+                // Tulostetaan konsoliin tulokset
+                console.log("Reseptejä löytyi:", data.meals.length);
+                    for (let i = 0; i < data.meals.length; i++) {
+                        console.log(data.meals[i].strMeal);
+                    }
+
+                // Poistetaan vanhat hakutulokset
+                $("#tulokset").empty();
+
+                    // Näytetään tulokset-elementissä
+                    for (let i = 0; i < data.meals.length; i++) {
+                        let meal = data.meals[i];
+                        let mealURL = "resepti.html?id=" + meal.idMeal;
+                        
+                        // Luodaan reseptistä korttinäkymä, reseptikortti linkkinä reseptiin
+                        $("#tulokset").append(`
+                            <div class = "reseptikortti"> 
+                                <a href = "${mealURL}"> 
+                                    <img src = "${meal.strMealThumb}" alt = "${meal.strMeal}">
+                                    <h2>${meal.strMeal}</h2>
+                                </a>    
+                                <p>Category: ${meal.strCategory}</p>
+                                <p>Region: ${meal.strArea}</p>
+                            </div>
+                        `);
+                        }
+                    })
+                    .catch((error) => {
+                        if (error.response) {
+                            console.error(error.response.data);
+                            console.error(error.response.status);
+                            console.error(error.response.headers);
+                        } else if (error.request) {
+                            console.error(error.request);
+                        } else {
+                            console.error("Error", error.message);
+                        }
+                        $("#tulokset").text("There was an error while fetching recipes. Please try again later.")
+                    });
+                });
+            });
+
+// Tekstikenttä: API -kutsu tapahtuu Enter-näppäintä painamalla, haetaan reseptin nimen mukaan
 $(document).ready(function() {
     $(".hakukentta").keypress(function(e) {
         if (e.key === "Enter") {
@@ -31,9 +106,11 @@ $(document).ready(function() {
                     for (let i = 0; i < data.meals.length; i++) {
                         console.log(data.meals[i].strMeal);
                     }
+                    
+                    // Poistetaan vanhat hakutulokset
+                    $("#tulokset").empty();
 
                     // Näytetään hakutulokset div-elementissä
-                    $("#tulokset").empty(); // Poistetaan vanhat hakutulokset
                     for (let i = 0; i < data.meals.length; i++) {
                         let meal = data.meals[i];
                         let mealURL = "resepti.html?id=" + meal.idMeal;
